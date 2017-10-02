8a660e604e668969af17055846cb876f8e69f9a9
Marcos Jota <marcosajc@gmail.com>
2017-10-01 20:17:23 -0400

    BoundingBox class

---
+++ Assets/Scripts/BoundingBox.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox : Forms {
+
+	private float orientation;
+
+	public BoundingBox ( List<Vector2> Vertex ) : base( Vertex ) {}
+
+	public BoundingBox ( Vector2 circleCenter, float circleRadius ) : base( circleCenter, circleRadius ) {}
+
+	public bool rayCollides ( Vector2 startPoint, Vector2 endPoint ) {
+
+		bool collides = false;
+		bool condMayor = true;
+		bool condMenor = true;
+		Vector2 auxStartVector;
+		Vector2 auxEndVector;
+
+		float m;	// Pendiente de la recta
+		float b;	// Punto de corte
+
+		// Determinamos si la la recta puede pasar por el rectángulo. Si los puntos que forman el vector
+		// no poseen al menos una COORDENADA de un vertice entre ellos, no PUEDE estar dentro del rectángulo.
+		for (int i = 0; (i < countVertex) && !collides ; i++) {
+
+			auxStartVector = (startPoint - vertex [i]);
+			auxEndVector = (endPoint - vertex [i]);
+
+			collides = collides || ( auxStartVector.x * auxEndVector.x <= 0) || (auxStartVector.x * auxEndVector.x <= 0);
+		}
+
+		// Si no se cumple la condición mínma para existir colisión, salgo.
+		if (!collides)
+			return false;
+
+		// Si alguno de los puntos pertenece al polígono, hay colisión.
+		if (belongsTo (startPoint) || belongsTo (endPoint))
+			return true;
+
+		// Determino la ecuación de la recta
+		m = (endPoint.y - startPoint.y) / (endPoint.x - startPoint.x);
+		b = -m * startPoint.x + startPoint.y;
+
+		// Determino si todos los vértices pertenecen a la región mayor o menor a la recta
+		// Si al menos uno de los puntos no pertenece a la región, el bool se torna falso.
+
+		// Nota: Se utilizan los dos dado que son opuestos. Si todos están en una región
+		// puede generar false. De esta forma ambas deben ser false para poder existir colisión.
+		for (int i = 0; i < countVertex; i++) {
+			condMayor = condMayor && ( vertex[i].y > m * vertex[i].x + b);
+			condMenor = condMayor && ( vertex[i].y < m * vertex[i].x + b);
+		}
+
+		// Si se cumple la condición anterior, existe colisión.
+		collides = collides && !condMayor && !condMenor;
+
+		return collides;
+	}
+
+}
---
+++ Assets/Scripts/Forms.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forms {
+
+	protected Vector2 center;
+	protected float radius;
+
+	protected List<Vector2> vertex;
+	protected int countVertex;
+	protected List<Vector2> sides;
+	protected static string[] type = new string[] { "circle", "dot", "line", "triangle", "rectangle" };
+
+	public Forms( List<Vector2> Vertex ){
+
+		vertex = Vertex;
+		countVertex = vertex.Count;
+
+	}
+	
+	public Forms( Vector2 circleCenter, float circleRadius ){
+
+		countVertex = 0;
+		center = circleCenter;
+		radius = circleRadius;
+
+	}
+
+	public bool belongsTo( Vector2 point ){
+
+		if (countVertex == 0) { // Es un circulo.
+
+			Vector2 newPoint = point - center;
+
+			return (Mathf.Pow (newPoint.x, 2) + Mathf.Pow (newPoint.y, 2) == Mathf.Pow (radius, 2));
+		} else {
+
+			Forms insideTriangle = new Forms ( new List<Vector2> { vertex[countVertex-1], point, vertex[0]});
+
+			float areaWithPoint = insideTriangle.getArea ();
+
+			for (int i = 0; i < countVertex-1; i++){
+
+				insideTriangle.setVertex (new List<Vector2> { vertex[i], point, vertex[i+1] });
+				areaWithPoint += insideTriangle.getArea ();
+
+			}
+
+			return !(areaWithPoint > getArea ());
+
+
+		}
+	}
+
+	private float getArea(){
+
+		float area = (vertex [0].x - vertex [countVertex - 1].x) * (vertex [0].y + vertex [countVertex - 1].y) / 2;
+
+		for (int i = 0; i < countVertex-1; i++)
+			area += (vertex[i+1].x - vertex[i].x) * (vertex[i+1].y - vertex[i].y) / 2;
+
+		return Mathf.Abs(area);
+
+	}
+
+	public void setVertex( List<Vector2> newVertex ){
+
+		vertex = newVertex;
+		countVertex = vertex.Count;
+
+	}
+}
